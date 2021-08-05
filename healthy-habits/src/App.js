9236030eb0361ba605c360eeb5fecbc17e0b9c21
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-08-04 20:17:35 -0400

    Pass in numbers as prop

--- healthy-habits/src/App.js
+++ healthy-habits/src/App.js
@@ -4,92 +4,89 @@ import Details from "./components/details.js";
 import "./App.css";
 
 function App() {
+  // TODO: Create a state variable to hold the five numbers to track. This should be an object with five properties: steps, water, food, exercise, and net. They should each be initialized with with a zero value (as a number).  Notice the hook you need has been imported at the top.
+  const [data, setData] = useState({
+    steps: 0,
+    water: 0,
+    food: 0,
+    exercise: 0,
+    net: 0,
+  });
 
-    // TODO: Create a state variable to hold the five numbers to track. This should be an object with five properties: steps, water, food, exercise, and net. They should each be initialized with with a zero value (as a number).  Notice the hook you need has been imported at the top.
+  // TODO: Create a state variable to track which tab should be displayed.
+  // Initialize it to the string "net".
 
+  // TODO: Inside each of the following handlers, update the corresponding numbers property from state.
+  const handleUpdateSteps = (amount) => {
+    // You can use your setter to update the state variable directly with the new amount.
+  };
+  const handleUpdateWater = (amount) => {
+    // You can use your setter to update the state variable directly with the new amount.
+  };
+  const handleUpdateFood = (amount) => {
+    // You'll need to update both the food calories and the net calories here (add calories consumed). Use prevState to ensure you are adding the new amount to the existing total.
+  };
+  const handleUpdateExercise = (amount) => {
+    // You'll need to update both the food calories and the net calories here (subtract calories burned). Use prevState to ensure you are adding the new amount to the existing total.
+  };
 
-    // TODO: Create a state variable to track which tab should be displayed.
-    // Initialize it to the string "net".
-    
+  // TODO: Change the current type of details to be displayed.
+  const handleCurrTypeChange = (type) => {
+    // Use the setter from state.
+  };
 
-    // TODO: Inside each of the following handlers, update the corresponding numbers property from state.
-    const handleUpdateSteps = (amount) => {
-        // You can use your setter to update the state variable directly with the new amount.
-        
-    };
-    const handleUpdateWater = (amount) => {
-        // You can use your setter to update the state variable directly with the new amount.
+  // JSX to display all content on page
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Healthy Habits</h1>
+      </header>
 
-    };
-    const handleUpdateFood = (amount) => {
-        // You'll need to update both the food calories and the net calories here (add calories consumed). Use prevState to ensure you are adding the new amount to the existing total.
+      {/* TODO: Pass the numbers object into the Chart component as a prop */}
+      <Chart numbers={data} />
 
-    };
-    const handleUpdateExercise = (amount) => {
-        // You'll need to update both the food calories and the net calories here (subtract calories burned). Use prevState to ensure you are adding the new amount to the existing total. 
-
-    };
-
-    // TODO: Change the current type of details to be displayed.
-    const handleCurrTypeChange = (type) => {
-        // Use the setter from state.
-        
-    };
-
-    // JSX to display all content on page
-    return (
-        <div className="app">
-
-            <header className="app-header">
-                <h1>Healthy Habits</h1>
-            </header>
-
-            {/* TODO: Pass the numbers object into the Chart component as a prop */}
-            <Chart />
-
-            <div className="tabs-container">
-                <div
-                    className="tab net-bkg"
-                    onClick={() => handleCurrTypeChange("net")}
-                >
-                    INFO
-                </div>
-
-                <div
-                    className="tab water-bkg"
-                    onClick={() => handleCurrTypeChange("water")}
-                >
-                    WATER+
-                </div>
-
-                <div
-                    className="tab steps-bkg"
-                    onClick={() => handleCurrTypeChange("steps")}
-                >
-                    STEPS+
-                </div>
-
-                <div
-                    className="tab food-bkg"
-                    onClick={() => handleCurrTypeChange("food")}
-                >
-                    FOOD+
-                </div>
-
-                <div
-                    className="tab exercise-bkg"
-                    onClick={() => handleCurrTypeChange("exercise")}
-                >
-                    EXERCISE+
-                </div>
-            </div>
-            
-            {/* TODO: Pass the current type into the Details component */}
-            {/* TODO: Pass the four update handler functions to the Details component below. Check the propTypes object at the bottom of Details.js to get the prop names, then look just below the state variables in this file to get the names of the handler functions. */}
-            <Details />
-            
+      <div className="tabs-container">
+        <div
+          className="tab net-bkg"
+          onClick={() => handleCurrTypeChange("net")}
+        >
+          INFO
         </div>
-    );
+
+        <div
+          className="tab water-bkg"
+          onClick={() => handleCurrTypeChange("water")}
+        >
+          WATER+
+        </div>
+
+        <div
+          className="tab steps-bkg"
+          onClick={() => handleCurrTypeChange("steps")}
+        >
+          STEPS+
+        </div>
+
+        <div
+          className="tab food-bkg"
+          onClick={() => handleCurrTypeChange("food")}
+        >
+          FOOD+
+        </div>
+
+        <div
+          className="tab exercise-bkg"
+          onClick={() => handleCurrTypeChange("exercise")}
+        >
+          EXERCISE+
+        </div>
+      </div>
+
+      {/* TODO: Pass the current type into the Details component */}
+      {/* TODO: Pass the four update handler functions to the Details component below. Check the propTypes object at the bottom of Details.js to get the prop names, then look just below the state variables in this file to get the names of the handler functions. */}
+      <Details />
+    </div>
+  );
 }
 
 export default App;
--- healthy-habits/src/components/chart.js
+++ healthy-habits/src/components/chart.js
@@ -3,51 +3,52 @@ import PropTypes from "prop-types";
 import "../App.css";
 
 const Chart = (props) => {
+  // TODO: Replace the hard-coded example numbers below with props coming from the parent, App
 
-    // TODO: Replace the hard-coded example numbers below with props coming from the parent, App
-
-    // JSX that will display the Chart component
-    return (
-        <React.Fragment>
-            <div className="circle">
-                <div className="net-number cell-number">1530</div>
-                <div className="cell-category">
-                    calories<br /> (net)
-                </div>
-            </div>
-            <table className="chart-table">
-                <tbody>
-                    <tr className="chart-row">
-                        <td className="cell-q2 chart-cell">
-                            <div className="cell-number">4384</div>
-                            <div className="cell-category">
-                                steps<br /> taken
-                            </div>
-                        </td>
-                        <td className="cell-q1 chart-cell">
-                            <div className="cell-number">7</div>
-                            <div className="cell-category">glasses of water</div>
-                        </td>
-                    </tr>
-                    <tr className="chart-row">
-                        <td className="cell-q3 chart-cell">
-                            <div className="cell-number">1840</div>
-                            <div className="cell-category">calories consumed</div>
-                        </td>
-                        <td className="cell-q4 chart-cell">
-                            <div className="cell-number">310</div>
-                            <div className="cell-category">calories burned</div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </React.Fragment>
-    );
+  // JSX that will display the Chart component
+  return (
+    <React.Fragment>
+      <div className="circle">
+        <div className="net-number cell-number">{props.numbers.net}</div>
+        <div className="cell-category">
+          calories
+          <br /> (net)
+        </div>
+      </div>
+      <table className="chart-table">
+        <tbody>
+          <tr className="chart-row">
+            <td className="cell-q2 chart-cell">
+              <div className="cell-number">{props.numbers.steps}</div>
+              <div className="cell-category">
+                steps
+                <br /> taken
+              </div>
+            </td>
+            <td className="cell-q1 chart-cell">
+              <div className="cell-number">{props.numbers.water}</div>
+              <div className="cell-category">glasses of water</div>
+            </td>
+          </tr>
+          <tr className="chart-row">
+            <td className="cell-q3 chart-cell">
+              <div className="cell-number">{props.numbers.food}</div>
+              <div className="cell-category">calories consumed</div>
+            </td>
+            <td className="cell-q4 chart-cell">
+              <div className="cell-number">{props.numbers.exercise}</div>
+              <div className="cell-category">calories burned</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </React.Fragment>
+  );
 };
 
 // This provides validation of data types for all props
 Chart.propTypes = {
-    numbers: PropTypes.object.isRequired,
+  numbers: PropTypes.object.isRequired,
 };
 
 export default Chart;
